060b85421bcdf849ed7f884d9e06efcc6347d724
leejm521 <leejm521@gmail.com>
2024-07-24 14:03:03 +0900

    samyang 첫 업로드

---
+++ samyang.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(".gnb").mouseenter(function(){
+        $(this).find(".sub").stop().slideDown();
+    });
+    $(".gnb").mouseleave(function(){
+        $(this).find(".sub").stop().slideUp();
+    });
+
+
+    $(".menu").click(function(){
+        $(".side_menu").stop().animate({"left":"0%"});
+    });
+    $(".close").click(function(){
+        $(".side_menu").stop().animate({"left":"-70%"});
+    });
+});
